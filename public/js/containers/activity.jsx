30475540ba907c4f5175b1259ba36e8b8eb5ed68
ttm1017 <463786395@qq.com>
2016-10-17 16:56:55 +0800

    add initial css,and adjust component strcture

---
+++ public/js/containers/activity.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+export default class Activity extends React.Component {
+    render(){
+        this.props.content();
+        return (
+            <div>
+                dfsf
+            </div>
+        )
+    }
+}
---
+++ public/js/containers/user.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+//import
+export default class activity extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (<div>
+            user
+        </div>)
+    }
+}
